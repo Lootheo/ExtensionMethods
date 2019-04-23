c9d1047362bed79f27e73ae878f3603eb96a39a2
Manuel Otheo <lootheo7@gmail.com>
2019-04-23 18:47:42 -0500

    second commmit

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static void TurnRed(this GameObject gameObjectToTurnRed)
+    {
+        gameObjectToTurnRed.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    public static void TurnWhite(this GameObject gameObjectToTurnWhite)
+    {
+        gameObjectToTurnWhite.GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+    public static void MoveUp(this Transform transformToMoveUp, float amount)
+    {
+        transformToMoveUp.position += Vector3.up * amount;
+    }
+    public static void MoveDown(this Transform transformToMoveUp, float amount)
+    {
+        transformToMoveUp.position += Vector3.down * amount;
+    }
+}
---
+++ Assets/Scripts/ExtensionMethodsExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ExtensionMethodsExample : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ColorChanger());
+
+        transform.MoveUp(5.0f);
+        
+    }
+
+    IEnumerator ColorChanger()
+    {
+        while (true)
+        {
+            cube.TurnRed();
+            yield return new WaitForSeconds(1.0f);
+            cube.TurnWhite();
+            yield return new WaitForSeconds(1.0f);
+            cube.transform.MoveUp(3.0f);
+            yield return new WaitForSeconds(1.0f);
+            cube.transform.MoveDown(3.0f);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+}
